cb477fbda61c6f85027a8d1f86b9fdbef96c5b21
edwardrutz <edward-github@opayq.com>
2019-10-07 21:42:19 -0400

    Added data driven circles

---
+++ README.md
@@ -0,0 +1,25 @@
+# D3 Basics
+
+
+
+## Notes
+
+- Single Vector Graphics (SVG)
+- The SVG element, `<svg></svg>` is the canvas for the SVG.  
+- Any SVG outside the canvas will not appear.
+- SVG will appear "clipped" if it goes over the edge of the canvas.
+- A stroke is a border drawn around the outside of the shape.
+- SVG rectangles/squares start from the top-left coordinates.
+- Circles start from the center coordinate.
+```svg
+<svg width="400" height="60">
+  <circle cx="90" cy="25" r="25" fill="red"></cirle>
+</svg>
+```
+
+
+
+
+## More Information
+
+- [Udemy: Mastering data visualization in D3.js](https://www.udemy.com/course/masteringd3js/)
--- js/main.js
+++ js/main.js
@@ -3,11 +3,35 @@
 * Data Visualization with D3.js
 */
 
-
+// Circles representing data in an array
+var data = [25, 20, 10, 12, 15];
 
 var svg = d3.select("#chart-area").append("svg")
     .attr("width", 400)
     .attr("height", 400);
+
+var circles = svg.selectAll("circle")
+  .data(data);
+
+circles.enter()
+  .append("circle")
+    .attr("cx", (d, i) => {
+      console.log("Item: " + d,  "Index: " + i);
+      return (i * 50) + 25;
+    })
+    .attr("cy", 25)
+    .attr("r", (d) => {
+        console.log("Item: " + d);
+        return d;
+    })
+    .attr("fill", "red");
+
+
+
+// A basic circle
+var svg = d3.select("#basic-circle").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
 
 var circle = svg.append("circle")
     .attr("cx", 100)
